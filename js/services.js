4db307af41030809fcf894697b265adb793b6208
geodevmarketing <cryptdata1234567892@gmail.com>
2025-12-14 11:03:29 +0100

    Update services.js

--- js/services.js
+++ js/services.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 "Analyse et interrogation des données spatiales en temps réel",
                 "Partage et diffusion de l’information géographique pour l’aide à la décision"
             ],
-            video: "media/LGeoCRUD2.mp4",
+            video: "media/LGeoCRUD2_1.mp4",
             video2: "media/frontTrim.mp4"
         },
         visite: {
